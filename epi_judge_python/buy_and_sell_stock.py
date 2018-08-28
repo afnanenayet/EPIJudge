6faa07576858f5019050939ffc4f3ad43b79738c
Afnan Enayet <afnan.enayet@gmail.com>
2018-08-28 16:05:18 -0400

    Implement solution for 5.6

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,12 +2,18 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+    diff = 0
+    curr_min = prices[0]
+
+    for price in prices:
+        diff = max(diff, price - curr_min)
+        curr_min = min(curr_min, price)
+    return max(0.0, diff)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("buy_and_sell_stock.py",
-                                       "buy_and_sell_stock.tsv",
-                                       buy_and_sell_stock_once))
+        generic_test.generic_test_main(
+            "buy_and_sell_stock.py", "buy_and_sell_stock.tsv", buy_and_sell_stock_once
+        )
+    )
