3c8266e4f8974a44a439e8eb1051696e46c19278
Afnan Enayet <afnan.enayet@gmail.com>
2018-08-28 16:43:44 -0400

    Implement 6.1

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -2,14 +2,43 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
-def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+def int_to_string(x: int) -> str:
+    is_neg = False
+    s = ""
+
+    if x < 0:
+        is_neg = True
+        x *= -1
+
+    # special case if x is 0
+    if x == 0:
+        return "0"
+
+    while x:
+        d = x % 10
+        s = str(d) + s
+        x = x // 10
+
+    if is_neg:
+        s = "-" + s
+    return s
 
 
-def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+def string_to_int(s: str) -> int:
+    num = 0
+    is_neg = False
+
+    if s[0] == "-":
+        s = s[1:]
+        is_neg = True
+
+    for i, c in enumerate(s):
+        power = len(s) - i - 1
+        num += int(c) * (10 ** power)
+
+    if is_neg:
+        num *= -1
+    return num
 
 
 def wrapper(x, s):
@@ -19,8 +48,11 @@ def wrapper(x, s):
         raise TestFailure("String to int conversion failed")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("string_integer_interconversion.py",
-                                       'string_integer_interconversion.tsv',
-                                       wrapper))
+        generic_test.generic_test_main(
+            "string_integer_interconversion.py",
+            "string_integer_interconversion.tsv",
+            wrapper,
+        )
+    )
