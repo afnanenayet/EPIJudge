b5fb87837cfb412d29cc420022c314e6c975645a
Afnan Enayet <afnan.enayet@gmail.com>
2018-08-28 23:03:04 -0400

    Partially implement 10.1

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,13 +1,36 @@
 from test_framework import generic_test
+import heapq
 
 
 def merge_sorted_arrays(sorted_arrays):
-    # TODO - you fill in here.
-    return []
+    """ Add each element from the sorted arrays to the heap so it can sort
+    them, and we can iterate through an arbitrary number of arrays and end
+    up with a sorted union
+    """
+    h = list()
+    first_run = True
+    res = list()
+
+    iters = [iter(array) for array in sorted_arrays]
+    finished = [False for _ in range(len(iters))]
+
+    # add next element from each array to heap, extract min and add to the
+    # result
+    while not all(finished):
+        for i, it in enumerate(iters):
+            try:
+                n = next(it)
+                heapq.heappush(h, n)
+            except StopIteration:
+                finished[i] = True
+        while h:
+            res.append(heapq.heappop(h))
+    return res
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("sorted_arrays_merge.py",
-                                       "sorted_arrays_merge.tsv",
-                                       merge_sorted_arrays))
+        generic_test.generic_test_main(
+            "sorted_arrays_merge.py", "sorted_arrays_merge.tsv", merge_sorted_arrays
+        )
+    )
