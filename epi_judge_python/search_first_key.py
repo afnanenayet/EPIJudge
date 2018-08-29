29b50d9619fc9a6b5bd936984f86db1a13759a91
Afnan Enayet <afnan.enayet@gmail.com>
2018-08-28 23:24:32 -0400

    Implement 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,11 +2,28 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    L, R = 0, len(A) - 1
+
+    while L <= R:
+        # with Python's "big" integers, don't need to worry about buffer 
+        # overflow bug
+        M = (L + R) // 2
+
+        if A[M] > k:
+            R = M - 1
+        elif A[M] < k:
+            L = M + 1
+        # everything below this has the condition A[M] == k
+        elif M == 0 or A[M - 1] != k:
+            return M
+        else:
+            R = M - 1
+    return -1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "search_first_key.py", 'search_first_key.tsv', search_first_of_k))
+            "search_first_key.py", "search_first_key.tsv", search_first_of_k
+        )
+    )
