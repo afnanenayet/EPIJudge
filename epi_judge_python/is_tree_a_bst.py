8947540fd68c319b7b21dfb7f5da2b17e2adcf79
Afnan Enayet <afnan.enayet@gmail.com>
2018-08-30 00:17:44 -0400

    Implement 14.1

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,12 +1,32 @@
 from test_framework import generic_test
 
 
-def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
-    # TODO - you fill in here.
-    return True
+def is_binary_tree_bst(tree, low_range=float("-inf"), high_range=float("inf")):
+    """ A classic
+
+    Check if a binary tree is valid by specifying the base cases, and checking
+    if the children are valid.
+
+    complexity:
+        space: O(h) where h is the height of the tree
+        time: O(n)
+    """
+    # base case -- empty tree
+    if not tree:
+        return True
+
+    # Check if node is valid
+    if not (low_range <= tree.data <= high_range):
+        return False
+
+    # If node is valid, then tree is as valid as long as its children are valid
+    return is_binary_tree_bst(tree.left, low_range=low_range, high_range=tree.data) and \
+        is_binary_tree_bst(tree.right, low_range=tree.data, high_range=high_range)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main("is_tree_a_bst.py", 'is_tree_a_bst.tsv',
-                                       is_binary_tree_bst))
+        generic_test.generic_test_main(
+            "is_tree_a_bst.py", "is_tree_a_bst.tsv", is_binary_tree_bst
+        )
+    )
