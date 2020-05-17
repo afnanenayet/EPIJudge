889ca5c4200fceaa7de4bec0f4bd69a358a5d4ee
Afnan Enayet <afnan@blend.com>
2020-05-17 16:59:25 -0400

    Complete problem

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,27 @@
 from test_framework import generic_test
+import math
 
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    if x < 0:
+        return False
+    if x == 0:
+        return True
+
+    length = math.floor(math.log10(x))
+    left, right = 0, length
+
+    def digit_at(i: int) -> int:
+        """Get the digit at the ith position of a number (as if it were a
+        string).
+        """
+        return (x // (10 ** i)) % 10
+
+    while left < right:
+        if digit_at(left) != digit_at(right):
+            return False
+        left += 1
+        right -= 1
     return True
 
 
