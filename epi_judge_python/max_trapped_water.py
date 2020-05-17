0ad5878092aeaa3bc4d67827f051e3301adffb95
Afnan Enayet <afnan@blend.com>
2020-05-16 22:11:31 -0400

    Complete max trapped water

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -4,8 +4,22 @@ from test_framework import generic_test
 
 
 def get_max_trapped_water(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if len(heights) < 2:
+        return 0
+    max_height = 0
+    left, right = 0, len(heights) - 1
+
+    while left < right:
+        # The height is bounded by the lower of the two columns
+        height = min(heights[left], heights[right])
+        width = right - left
+        max_height = max(max_height, height * width)
+
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_height
 
 
 if __name__ == '__main__':
