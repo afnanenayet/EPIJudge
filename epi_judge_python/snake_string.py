fc8ed80f9abd07f992619dcd5bdefe4a21fdcda5
Afnan Enayet <afnan@blend.com>
2020-05-17 23:47:38 -0400

    Complete problem

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def snake_string(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    """Build each line separately
+    """
+    res = ""
+    offsets = [1, 0, 3]
+    intervals = [4, 2, 4]
+
+    for offset, interval in zip(offsets, intervals):
+        res += s[offset::interval]
+    return res
 
 
 if __name__ == '__main__':
