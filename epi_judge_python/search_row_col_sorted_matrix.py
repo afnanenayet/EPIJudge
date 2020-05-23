155b343e91fe0d3aa4e2774b16a0730aaf8e0399
Afnan Enayet <afnan@afnan.io>
2020-05-23 18:00:28 -0400

    Complete problem

--- epi_judge_python/search_row_col_sorted_matrix.py
+++ epi_judge_python/search_row_col_sorted_matrix.py
@@ -4,12 +4,28 @@ from test_framework import generic_test
 
 
 def matrix_search(A: List[List[int]], x: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not A:
+        return False
+    m = len(A)
+    n = len(A[0])
+    row_idx = 0
+    col_idx = len(A[0]) - 1
+
+    while 0 <= row_idx < m and 0 <= col_idx < n:
+        if x == A[row_idx][col_idx]:
+            return True
+        elif x < A[row_idx][col_idx]:
+            col_idx -= 1
+        else:
+            row_idx += 1
+    return False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('search_row_col_sorted_matrix.py',
-                                       'search_row_col_sorted_matrix.tsv',
-                                       matrix_search))
+        generic_test.generic_test_main(
+            "search_row_col_sorted_matrix.py",
+            "search_row_col_sorted_matrix.tsv",
+            matrix_search,
+        )
+    )
