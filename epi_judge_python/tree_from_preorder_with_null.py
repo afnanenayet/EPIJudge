c5bbc3443cdae02c85d533cbc5addfd5b6eb80d4
Afnan Enayet <afnan@afnan.io>
2020-05-23 17:04:13 -0400

    Complete problem

--- epi_judge_python/tree_from_preorder_with_null.py
+++ epi_judge_python/tree_from_preorder_with_null.py
@@ -1,5 +1,5 @@
 import functools
-from typing import List
+from typing import List, Optional
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
@@ -7,18 +7,30 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def reconstruct_preorder(preorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    def go(tree_iter) -> Optional[BinaryTreeNode]:
+        root = next(tree_iter)
+
+        if not root:
+            return None
+
+        left_subtree = go(tree_iter)
+        right_subtree = go(tree_iter)
+        return BinaryTreeNode(root, left_subtree, right_subtree)
+
+    return go(iter(preorder))
 
 
 @enable_executor_hook
 def reconstruct_preorder_wrapper(executor, data):
-    data = [None if x == 'null' else int(x) for x in data]
+    data = [None if x == "null" else int(x) for x in data]
     return executor.run(functools.partial(reconstruct_preorder, data))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('tree_from_preorder_with_null.py',
-                                       'tree_from_preorder_with_null.tsv',
-                                       reconstruct_preorder_wrapper))
+        generic_test.generic_test_main(
+            "tree_from_preorder_with_null.py",
+            "tree_from_preorder_with_null.tsv",
+            reconstruct_preorder_wrapper,
+        )
+    )
