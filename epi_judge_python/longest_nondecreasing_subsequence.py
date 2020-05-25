92d9e49aa4c3fe99d4ef28d1fe208107acd8f26f
Afnan Enayet <afnan@afnan.io>
2020-05-24 22:51:57 -0400

    Complete problem

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -4,13 +4,22 @@ from test_framework import generic_test
 
 
 def longest_nondecreasing_subsequence_length(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    # `max_length[i]` is the longest subarray in `A[:i]` up to,
+    # and including `A[i]`
+    max_length = [0 for _ in range(len(A))]
+
+    for i, elem in enumerate(A):
+        max_length[i] = 1 + max(
+            [x for (j, x) in enumerate(max_length[:i]) if A[j] <= elem] + [0]
+        )
+    return max(max_length)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            'longest_nondecreasing_subsequence.py',
-            'longest_nondecreasing_subsequence.tsv',
-            longest_nondecreasing_subsequence_length))
+            "longest_nondecreasing_subsequence.py",
+            "longest_nondecreasing_subsequence.tsv",
+            longest_nondecreasing_subsequence_length,
+        )
+    )
