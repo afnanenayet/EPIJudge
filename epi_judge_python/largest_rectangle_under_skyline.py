9276da53bc596429039017fe5585e3392504c194
Afnan Enayet <afnan@afnan.io>
2020-05-24 23:41:34 -0400

    Complete problem

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -4,12 +4,23 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack = []
+    curr_max = 0
+
+    for index, height in enumerate(heights + [0]):
+        while stack and heights[stack[-1]] >= height:
+            cand_height = heights[stack.pop()]
+            cand_width = index if not stack else (index - stack[-1] - 1)
+            curr_max = max(curr_max, cand_height * cand_width)
+        stack.append(index)
+    return curr_max
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('largest_rectangle_under_skyline.py',
-                                       'largest_rectangle_under_skyline.tsv',
-                                       calculate_largest_rectangle))
+        generic_test.generic_test_main(
+            "largest_rectangle_under_skyline.py",
+            "largest_rectangle_under_skyline.tsv",
+            calculate_largest_rectangle,
+        )
+    )
